bbc3d68b77967a9a606d10cca28b3e7ef53cf77f
JulianToledano <jtoledanodiaz@gmail.com>
2026-01-24 11:28:36 +0100

    add: ohcl array

--- candle.go
+++ candle.go
@@ -1,16 +1,67 @@
 package candlePrintGo
 
+// Candle represents a financial candlestick with OHLC (Open, High, Low, Close) data.
 type Candle interface {
 	Open() float64
 	High() float64
 	Low() float64
 	Close() float64
-	// Top returns what is the top of the candle either Open or Close
+	// Top returns the top of the candle body (either Open or Close, whichever is higher).
 	Top() float64
-	// Bottom returns what is the bottom of the candle either Open or Close
+	// Bottom returns the bottom of the candle body (either Open or Close, whichever is lower).
 	Bottom() float64
-	// IsBullish check the tendency of the candle
+	// IsBullish returns true if the candle is bullish (Close > Open).
 	IsBullish() bool
+}
+
+var _ Candle = &CandleBar{}
+var _ Candle = &OHCLArray{}
+
+// OHCLArray is a fixed-size array representing a candle with [Open, High, Low, Close] values.
+type OHCLArray [4]float64
+
+func (o OHCLArray) Open() float64 {
+	return o[0]
+}
+
+func (o OHCLArray) High() float64 {
+	return o[1]
+}
+
+func (o OHCLArray) Low() float64 {
+	return o[2]
+}
+
+func (o OHCLArray) Close() float64 {
+	return o[3]
+}
+
+func (o OHCLArray) IsBullish() bool {
+	return o[3] > o[0]
+}
+
+func (o OHCLArray) Top() float64 {
+	if o.IsBullish() {
+		return o[3] // close
+	}
+	return o[0] // open
+}
+
+func (o OHCLArray) Bottom() float64 {
+	if o.IsBullish() {
+		return o[0] // open
+	}
+	return o[3] // close
+}
+
+// NewOHCLArray creates a new OHCLArray from individual Open, High, Low, Close values.
+func NewOHCLArray(o, h, l, c float64) *OHCLArray {
+	return &OHCLArray{o, h, l, c}
+}
+
+// NewOHCLArrayFromArray creates a new OHCLArray from a slice where elements are [Open, High, Low, Close].
+func NewOHCLArrayFromArray(arr []float64) *OHCLArray {
+	return &OHCLArray{arr[0], arr[1], arr[2], arr[3]}
 }
 
 type CandleBar struct {
@@ -31,6 +82,15 @@ func NewCandleBar(o, h, l, c float64) *CandleBar {
 	}
 }
 
+func NewCandleBarFromArray(arr []float64) *CandleBar {
+	return &CandleBar{
+		isBullish: arr[3] > arr[0],
+		open:      arr[0],
+		high:      arr[1],
+		low:       arr[2],
+		close:     arr[3],
+	}
+}
 func (c CandleBar) Open() float64 {
 	return c.open
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,19 @@ module github.com/bitcolibri/candlePrintGo
 
 go 1.20
 
-require github.com/muesli/termenv v0.15.1
+require (
+	github.com/muesli/termenv v0.15.1
+	github.com/stretchr/testify v1.11.1
+)
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
